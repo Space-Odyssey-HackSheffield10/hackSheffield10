685c863ecda2f28a2e4f5e9e220f1538773e3446
Gheist404 <thomasjforster@outlook.com>
2025-11-29 16:53:04 +0000

    let's see app.html

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
+app.mount("/app/src", StaticFiles(directory="app/src"), name="app/src")
+templates = Jinja2Templates(directory="app/src")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse("app.html", {"request": request})
